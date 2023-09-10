8673a5ae49c501b1e65cc15b50c3943269458b22
LucasNava <navarrolucas4668@gmail.com>
2023-09-10 10:37:07 -0300

    Implementacion del modal para el filtrado

--- Vistas/V_principal.php
+++ Vistas/V_principal.php
@@ -9,9 +9,9 @@
 <div class="ContenedorProductos">
     <?php
     while ($fila = mysqli_fetch_assoc($datosProductos)){?>
-<div class="card" style="width: 13rem;">
+<div class="card" style="width: 18rem;">
   <img src="<?php echo $fila["img"];?>" class="card-img-top" alt="...">
-  <div class="card-body "style="width: 13rem;">
+  <div class="card-body">
     <h5 class="card-title"><?php echo $fila["nombre"];?>
 </h5>
     <p class="card-text"><?php echo $fila["precio"];?></p>
--- Vistas/navbar.php
+++ Vistas/navbar.php
@@ -27,10 +27,39 @@
           <a class="nav-link" href="#">COMO COMPRAR</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+          <a class="nav-link" href="#">
+          <!-- Button trigger modal -->
+<button type="button" class="botonModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<i class="fas fa-search"></i></a>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">¿Que estas buscando?</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+      <div>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="filtrado" class="form-text"></div>
+    </div>
+
+    </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>  
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+          <a class="nav-link" href="#">            
+          <i class="fas fa-shopping-cart"></i></a>
         </li>
         
       </ul>
