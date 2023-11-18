480b4b15212df40a2db19ba801a6196eb11f6295
hcozzani <hugocozzani@gmail.com>
2023-11-18 19:51:57 -0300

    arregle el error de mp cuando en el carrito se eliminaban todos los productos y quedaba en cero

--- Controlador/C_mercadoPago.php
+++ Controlador/C_mercadoPago.php
@@ -1,11 +1,13 @@
 <?php
+
     require_once ("../vendor/autoload.php");
 
     use MercadoPago\Client\Preference\PreferenceClient;
     use MercadoPago\MercadoPagoConfig;
 
     MercadoPagoConfig::setAccessToken("TEST-4808156112961165-102416-f319912a4a575a0ed10c567e5d0a2bd5-1520338902");
-    $total = 0;
+   
+    if (isset($total)) {
     $client = new PreferenceClient();
     $preference = $client->create([
     "items"=> array(
@@ -18,13 +20,12 @@
         )
     )
     ]);
-    //$preference->save();
     $preference->auto_return = "approved";
     $preference->binary_mode = true;
 
     $preference->back_urls = array(
         "success" => "",
         "failure" => ""
-    );
+    );}
 
 ?>
