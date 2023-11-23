43b4d12133a3818c7d18d6811b1c280ef1e45429
eliasalegreok <eliasrafaelalegre@gmail.com>
2023-11-23 14:28:21 -0300

    Arreglo de MP
    -Se corrigió parte del codigo de C_mercadoPago.php por el problema que al finalizar el pago no volvia a la pagina de la tienda.

--- Controlador/C_mercadoPago.php
+++ Controlador/C_mercadoPago.php
@@ -8,42 +8,27 @@
 
      MercadoPagoConfig::setAccessToken("TEST-4808156112961165-102416-f319912a4a575a0ed10c567e5d0a2bd5-1520338902");
    
-     if(isset($total)){
-     $client = new PreferenceClient();
-     $preference = $client->create([
-     "items"=> array(
-         array(
-         "id" => "01",
-         "title" => "Compra Amor & Moda",
-         "quantity" => 1,
-         "currency_id" => "ARS",
-         "unit_price" => $total
-         )
-     )
-     ]);
+     if (isset($total)) {
+        $client = new PreferenceClient();
+        $preference = $client->create([
+            "items" => array(
+                array(
+                    "id" => "01",
+                    "title" => "Compra Amor & Moda",
+                    "quantity" => 1,
+                    "currency_id" => "ARS",
+                    "unit_price" => $total
+                )
+            ),
+            "back_urls" => array(
+                "success" => "http://localhost/ecommerce/index.php",
+                "failure" => "http://localhost/ecommerce/index.php",
+                "pending" => "http://localhost/ecommerce/index.php"
+            ),
+            "auto_return" => "approved",
+            "binary_mode" => true
+        ]);
+    }
     
-     $preference->binary_mode = true;
-     $preference->back_urls = array(
-         "success" => "",
-         "failure" => ""
-     );
-     $preference->auto_return = "approved";
-     
- 
-     
-
-
-    //  $payment_url = $preference->sandbox_init_point;  // Cambiar a init_point en producción
-
-    //  // Agregar un parámetro a la URL de éxito
-    //  $success_url = "http://192.168.0.217/ecommerce/success.php?transaction_completed=true";
-          
-    //  // Redirigir al usuario a la URL de pago
-   
-    //  exit();
-     
-
-    ///////////////////////////////////
-}
 
 ?>
