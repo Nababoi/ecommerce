d5cdff2bd108e9a576bb546c07026198c1ebb276
hcozzani <hugocozzani@gmail.com>
2023-11-23 19:11:23 -0300

    facturacion

--- Controlador/C_facturacion.php
+++ Controlador/C_facturacion.php
@@ -1,39 +1,21 @@
 <?php
-require_once ("../vendor/autoload.php");
-require_once("../Controlador/C_mercadoPago.php");
-//factura en pdf
+require_once('../vendor/autoload.php');
+require("../Controlador/C_mercadoPago.php");
 
-// Verifica si $payment_info está definido y si su índice 'status' es 'approved'
-if (isset($payment_info) && $payment_info['status'] == 'approved') {
-    // Recupera la información de la venta desde donde la hayas almacenado (por ejemplo, en una base de datos)
-    $total;
+// Crear una nueva instancia de TCPDF
+$pdf = new TCPDF();
 
-    // Genera la factura en PDF
-    generateInvoice($total);
+// Agregar una página al PDF
+$pdf->AddPage();
 
-    // Puedes redirigir al usuario a una página de confirmación o simplemente mostrar un mensaje
-    echo "¡Venta exitosa! La factura se ha descargado automáticamente.";
-} else {
-    // La venta no fue exitosa, maneja según tus necesidades
-    echo "La venta no fue exitosa.";
+if(isset($total)){
+    $pdf->writeHTML("Precio total: $" . $total);
+
+    // Establecer el nombre del archivo PDF que se descargará
+    $filename = 'factura.pdf';
+
+    // Salida del PDF a la pantalla
+    $pdf->Output($filename, 'D');
 }
 
-function generateInvoice($total) {
-    // Crea una instancia de TCPDF
-    $pdf = new TCPDF();
-
-    // Agrega una página al PDF
-    $pdf->AddPage();
-
-    // Establece el contenido del PDF (puedes personalizar esto según tus necesidades)
-    $content = "
-    Detalles de la factura:
-    Total: $total
-    ";
-
-    $pdf->writeHTML($content, true, false, true, false, '');
-
-    // Guarda el PDF en el servidor o muestra en el navegador
-    $pdf->Output('factura.pdf', 'D'); // 'D' para descargar directamente en el navegador
-}
 ?>
--- Modelos/M_success.php
+++ Modelos/M_success.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once('../vendor/autoload.php');
+$pdf = new TCPDF();
+
 if (isset($_GET['status'])) {
     $status = $_GET['status'];
 
@@ -7,6 +10,8 @@ if (isset($_GET['status'])) {
         //CODIGO DE PRUEBA...
         echo "<h1>Compra exitosa en Mercado Pago</h1>";
         echo "<p>¡Gracias por tu compra!</p>";
+        echo '<a href="../Controlador/C_facturacion.php"><button>Descargar Factura</button></a>';
+
 
     } elseif ($status === 'rejected') {
         echo "<h1>Pago rechazado!</h1>";
@@ -15,39 +20,4 @@ if (isset($_GET['status'])) {
         echo "<h1>Pago pendiente!</h1>";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//ESTE CODIGO YA ESTABA EN ESTE ARCHIVO...
-// // Verificar si el parámetro 'transaction_completed' está presente
-// if (isset($_GET['transaction_completed']) && $_GET['transaction_completed'] === 'true') {
-//     // Obtener la URL de pago de la cookie
-//     $payment_url = isset($_COOKIE['payment_url']) ? $_COOKIE['payment_url'] : '';
-
-//     echo "<h1>Compra exitosa en Mercado Pago</h1>";
-//     echo "<p>¡Gracias por tu compra!</p>";
-
-//     // Mostrar el botón para volver al sitio
-//     if (!empty($payment_url)) {
-//         echo "<button onclick=\"window.location.href='$payment_url'\">Volver al sitio</button>";
-//     } else {
-//         echo "<p>Error: No se pudo obtener la URL de pago.</p>";
-//     }
-// } else {
-//     echo "<h1>Error en la transacción</h1>";
-// }
-
 ?>
